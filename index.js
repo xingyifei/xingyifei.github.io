01a0c7012b192fbeae782224ad40fe8be088d9b4
邢一飞 <xingyf@jointwisdom.cn>
2018-03-30 14:26:49 +0800

    no message

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 function Main() {
+  
+  // 轮播时间
+  Main.prototype.time = 3500
+  
+  // 当前图片
+  Main.prototype.count = 0
+  
   // 初始函数
   Main.prototype.init = function() {
-    
+    setInterval(this.slider.bind(this), this.time)
+  }
+  
+  // 轮播函数
+  Main.prototype.slider = function() {
+    this.count === ($('.slider').find('img').length - 1) && (this.count = -1)
+    this.count ++ 
+    $('.slider').find('img').eq(this.count).siblings().hide()
+    $('.slider').find('img').eq(this.count).fadeIn(1000)
   }
   
 }
